80b64e471ed35cdc72bdc345722f52ac98a251ba
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-11-25 15:16:01 -0300

    feat: fix ping method response struct

--- handler/api.go
+++ handler/api.go
@@ -4,8 +4,9 @@ import "github.com/gofiber/fiber/v2"
 
 // PING type function to see if its everything alright...
 func Hello(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"status":  "success",
-		"message": "Hello Stranger! so you found this api huh.",
+	return c.JSON(ResponseHTTP{
+		Success: false,
+		Message: "Hello Stranger! so you found this api huh.",
+		Data:    nil,
 	})
 }
