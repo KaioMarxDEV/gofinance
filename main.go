45d46ca91b89686e9e4f0f8d60c9857d8fc86f7c
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-11-25 14:50:47 -0300

    feat: add count limit to db connection call

--- main.go
+++ main.go
@@ -14,15 +14,18 @@ func Init() {
 	// connect to Docker infra using .env values
 	// tries until database docker is ready...this is a work around of Docker
 	// and distributed system issue with "waiting to someone"
-	connected := false
+	connected, try := false, 0
 	for connected != true {
+		if try > 5 {
+			break
+		}
 		connected = database.ConnectDB()
+		try++
 	}
 }
 
 func main() {
 	Init()
-
 	app := fiber.New()  // server instace creation by gofiber
 	app.Use(cors.New()) // enabling CORS for cross platform conections (Frontend)
 
