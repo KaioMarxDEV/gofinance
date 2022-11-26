3de65a5ce1c0f8eaf70ccbe1da3b4d9995585099
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-11-26 11:03:55 -0300

    tailwind css and front end project setup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        {count}
-        <button onClick={() => setCount(count+1)}>
+      <div className="flex justify-center align-top mt-8 bg-gray-400">
+        <p className='text-xs text-slate-800'>{count}</p>
+        <button className='p-3 rounded-xl' onClick={() => setCount(count+1)}>
           +1
         </button>
       </div>
