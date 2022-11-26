69fb473dec0bf06044aafea487cbe63d6b5d666c
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-11-26 10:12:47 -0300

    fix: api ping status bool

--- handler/api.go
+++ handler/api.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 // PING type function to see if its everything alright...
 func Hello(c *fiber.Ctx) error {
 	return c.JSON(ResponseHTTP{
-		Success: false,
+		Success: true,
 		Message: "Hello Stranger! so you found this api huh.",
 		Data:    nil,
 	})
