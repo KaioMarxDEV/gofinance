b6482648d8b508846c4632a3c6465368f3cbb8ec
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-11-28 11:02:55 -0300

    add: transactions table

--- web/src/components/Transactions/index.tsx
+++ web/src/components/Transactions/index.tsx
@@ -1,7 +1,17 @@
 export function Transactions() {
+  let variant = 'income'
   return (
-    <div className="w-full max-w-6xl px-6 mx-auto">
-      oi
+    <div className="w-full max-w-6xl mt-10 mx-auto px-5">
+      <table className="w-full">
+        <tbody className="flex flex-col gap-3">
+          <tr className="bg-gray-800 flex justify-between rounded-xl py-5 px-8">
+            <td className="w-1/2">Desenvolvimento de Software</td>
+            <td className={variant === 'income' ? 'text-green-300': 'text-red-300'}>-R$ 15.000,00</td>
+            <td>Vendas</td>
+            <td>18/03/2002</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,12 +1,28 @@
+import { MagnifyingGlass } from "phosphor-react";
 import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 import { Transactions } from "../components/Transactions";
 
 export function Home() {
   return (
-    <main className="h-screen">
+    <main>
       <Header />
       <Banner />
+      {/* Seach Bar */}
+      <div className="w-full max-w-6xl mx-auto px-5 flex flex-row">
+        <input
+          className="flex-1 mr-4 bg-gray-800 p-4 rounded-md"
+          placeholder="Transaction Name..."
+        />
+        <div>
+          <button className="flex flex-row justify-between items-center w-full rounded-md px-8 py-4 bg-gray-900 hover:bg-gray-900/50 border border-gray-800 hover:border-green-500 transition-all ease-in delay-75 duration-200">
+            <MagnifyingGlass className="text-green-500" size={16} />
+            <span className="ml-2 text-base text-green-500">
+              Search
+            </span>
+          </button>
+        </div>
+      </div>
       <Transactions />
     </main>
   )
