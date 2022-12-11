2d3baa5dc6daf1868ca8461e92445405ba3e296d
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-12-11 06:39:30 -0300

    add: adjust margin bottom on transactions tittle

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -21,7 +21,7 @@ export function Home() {
     <main>
       <Header />
       <Banner />
-      <div className="w-full max-w-6xl mx-auto px-5 flex flex-row mb-8">
+      <div className="w-full max-w-6xl mx-auto px-5 flex flex-row mb-4">
         <h2 className="font-bold text-4xl">
           Your Transaction<span className="text-green-500">$</span> :
         </h2>
